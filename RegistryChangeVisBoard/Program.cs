3ad9bd12c10cef82ca7baf8957e4bcc5f1d645f5
Defilan <defilan@gmail.com>
2014-04-10 15:45:39 -0700

    Added comments to code.

--- RegistryChangeVisBoard/Program.cs
+++ RegistryChangeVisBoard/Program.cs
@@ -9,6 +9,7 @@ namespace RegistryChangeVisBoard
     {
         static void Main()
         {
+            //This example reads from the file "VisBoards.txt" but it can be anything. This file contains a list of systems you want to run this reg change on. 
             int tempint = File.ReadAllLines("J:\\VisBoards.txt").Length;
             var system = new string[tempint];
             using (var sr = new StreamReader("J:\\VisBoards.txt"))
@@ -32,11 +33,13 @@ namespace RegistryChangeVisBoard
             {
                 using (RegistryKey regSec = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, system))
                 {
+                    //For this example, I've chosen to change a value which disables UAC on a remote Windows Vista/7/8/8.1 workstation.  
                     RegistryKey regSecKey = regSec.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System", true);
                     if (regSecKey != null) regSecKey.SetValue("EnableLUA", "0", RegistryValueKind.DWord);
                 }
                 using (var proc = new Process())
                 {
+                    //This will remotely reboot the remote workstation. 
                     proc.StartInfo.FileName = "shutdown.exe";
                     proc.StartInfo.Arguments = string.Format("-f -r -t 05 -m \\\\{0}", system);
                     proc.StartInfo.CreateNoWindow = true;
